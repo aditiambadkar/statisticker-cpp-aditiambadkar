8574c6c6fce1d7b4c7256d3afc9e6f2736ff36db
aditiambadkar <54082235+aditiambadkar@users.noreply.github.com>
2020-08-31 17:09:22 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -1,5 +1,13 @@
 #include <vector>
 
 namespace Statistics {
+    
+    class Stats {
+        private:
+            float average;
+            float max;
+            float min;
+    };   
     Stats ComputeStatistics(const std::vector<___>& );
+
 }
