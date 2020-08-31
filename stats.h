59cf9e682ea7f1970f3d4c087751d156d0f50ef3
aditiambadkar <54082235+aditiambadkar@users.noreply.github.com>
2020-08-31 17:58:18 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -3,10 +3,9 @@
 namespace Statistics {
     
     struct Stats {
-        public:
-            float average;
-            float max;
-            float min;
+        float average;
+        float max;
+        float min;
     };   
     Stats ComputeStatistics(const std::vector<float>& );
 }
