3849fff04da388e33854fdd31a7021a33da8bddd
aditiambadkar <54082235+aditiambadkar@users.noreply.github.com>
2020-08-31 20:22:24 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -2,20 +2,20 @@
 #include <bits/stdc++.h>
 namespace Statistics {
     
-    Stats ComputeStatistics(const std::vector<float>& v) {
+    Stats ComputeStatistics(const std::vector<float>& floatVector) {
         //Implement statistics here
-        Stats obj;
-        auto n = v.size();
-        if(v.size() == 0)
+        Stats statsObj;
+        auto size_floatVector = floatVector.size();
+        if(size_floatVector == 0)
         {
             obj.average = 0;
             obj.max = 0;
             obj.min = 0;
             return obj;
         }
-        obj.average = accumulate( v.begin(), v.end(), 0.0) / n;
-        obj.max = *max_element(v.begin(), v.end());
-        obj.min = *min_element(v.begin(), v.end());
-        return obj;
+        obj.average = accumulate( floatVector.begin(), floatVector.end(), 0.0) / size_floatVector;
+        obj.max = *max_element(floatVector.begin(), floatVector.end());
+        obj.min = *min_element(floatVector.begin(), floatVector.end());
+        return statsObj;
     }
 }
