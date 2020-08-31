e2f6b7240eb5fa6cb99618e89de69b92f237a540
aditiambadkar <54082235+aditiambadkar@users.noreply.github.com>
2020-08-31 17:44:09 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -6,6 +6,13 @@ namespace Statistics {
         //Implement statistics here
         Stats obj;
         auto n = v.size();
+        if(v.size == 0)
+        {
+            obj.average = 0;
+            obj.max = 0;
+            obj.min = 0;
+            return obj;
+        }
         obj.average = accumulate( v.begin(), v.end(), 0.0) / n;
         obj.max = *max_element(v.begin(), v.end());
         obj.min = *min_element(v.begin(), v.end());
