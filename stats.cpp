e70f9ddcabc121967c9970d93a1392ef7e2a3ae0
aditiambadkar <54082235+aditiambadkar@users.noreply.github.com>
2020-08-31 20:41:59 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -3,7 +3,6 @@
 namespace Statistics {
     
     Stats ComputeStatistics(const std::vector<float>& floatVector) {
-        //Implement statistics here
         Stats statsObj;
         auto size_floatVector = floatVector.size();
         /*if(size_floatVector == 0)
