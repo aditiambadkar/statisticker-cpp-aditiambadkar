ec1928a2f165cef57446102869ae14e7d60d8dd4
aditiambadkar <54082235+aditiambadkar@users.noreply.github.com>
2020-08-31 20:26:44 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -8,14 +8,14 @@ namespace Statistics {
         auto size_floatVector = floatVector.size();
         if(size_floatVector == 0)
         {
-            obj.average = 0;
-            obj.max = 0;
-            obj.min = 0;
-            return obj;
+            statsObj.average = 0;
+            statsObj.max = 0;
+            statsObj.min = 0;
+            return statsObj;
         }
-        obj.average = accumulate( floatVector.begin(), floatVector.end(), 0.0) / size_floatVector;
-        obj.max = *max_element(floatVector.begin(), floatVector.end());
-        obj.min = *min_element(floatVector.begin(), floatVector.end());
+        statsObj.average = accumulate( floatVector.begin(), floatVector.end(), 0.0) / size_floatVector;
+        statsObj.max = *max_element(floatVector.begin(), floatVector.end());
+        statsObj.min = *min_element(floatVector.begin(), floatVector.end());
         return statsObj;
     }
 }
