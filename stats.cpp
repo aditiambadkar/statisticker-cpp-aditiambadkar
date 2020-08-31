b85f2c9f59c58ab27f72dc27f14f6ae96eab268c
aditiambadkar <54082235+aditiambadkar@users.noreply.github.com>
2020-08-31 20:32:04 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -5,14 +5,14 @@ namespace Statistics {
     Stats ComputeStatistics(const std::vector<float>& floatVector) {
         //Implement statistics here
         Stats statsObj;
-        auto size_floatVector = floatVector.size();
+        /*auto size_floatVector = floatVector.size();
         if(size_floatVector == 0)
         {
             statsObj.average = 0;
             statsObj.max = 0;
             statsObj.min = 0;
             return statsObj;
-        }
+        }*/
         statsObj.average = accumulate( floatVector.begin(), floatVector.end(), 0.0) / size_floatVector;
         statsObj.max = *max_element(floatVector.begin(), floatVector.end());
         statsObj.min = *min_element(floatVector.begin(), floatVector.end());
