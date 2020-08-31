f7b8bd47676c8be163bcc239ceb32fd2dedaf444
aditiambadkar <54082235+aditiambadkar@users.noreply.github.com>
2020-08-31 16:59:45 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -1,5 +1,18 @@
 #include "stats.h"
+class Stats
+{
+    private:
+    float average;
+    float max;
+    float min;
+};
 
-Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
+Stats Statistics::ComputeStatistics(const std::vector<___>& v) {
     //Implement statistics here
+    Stats obj;
+    auto n = v.size();
+    obj.average = accumulate( v.begin(), v.end(), 0.0) / n;
+    obj.max = *max_element(v.begin(), v.end());
+    obj.min = *min_element(v.begin(), v.end());
+    return obj;
 }
